92fbedaaf2e4de578a1d18ff25a44258bea7d805
Koen Molenaar <koen.molenaar26@gmail.com>
2020-09-15 18:48:16 +0200

    Adding Pipfile

---
+++ server/settings/prod.py
@@ -0,0 +1,14 @@
+""" Production Settings """"
+
+import os
+import dj_database_url
+from .dev import *
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=environ('DATABASE_URL')
+    )
+}
+
+ALLOWED_HOSTS = [env('DOMAIN')]
